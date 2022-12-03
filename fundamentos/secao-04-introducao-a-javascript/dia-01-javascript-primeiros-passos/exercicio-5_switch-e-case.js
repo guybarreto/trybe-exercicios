238b2a087f15ff4288dd40ed0e451e92376fe17e
guybarreto <guybarreto@icloud.com>
2022-12-02 23:48:45 -0300

    Create exercicio-5_switch-e-case.js

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-5_switch-e-case.js
@@ -0,0 +1,9 @@
+// Exercício 05:
+
+// 01. Crie uma variável para armazenar o status da pessoa candidata no processo seletivo, que pode ser: 'aprovada', 'lista' ou 'reprovada';
+
+// 02. Crie uma estrutura condicional com o switch/case que irá imprimir as seguintes mensagens:
+//      * Caso 'aprovada', imprima “Parabéns, você foi aprovada(o)!”.
+//      * Caso 'lista', imprima “Você está na nossa lista de espera”.
+//      * Caso 'reprovada', imprima “Você foi reprovada(o)”.
+//      * Caso default, imprima a mensagem de “Informação incorreta”.
