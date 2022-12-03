eac49cd5679a010b8a5f3892b25fb3908359140f
guybarreto <guybarreto@icloud.com>
2022-12-03 18:06:58 -0300

    Create exercicio-6_chess-movements.js

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-6_chess-movements.js
@@ -0,0 +1,6 @@
+// Exercício 06:
+// 1. Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+// 2. Como desafio, escreva um código para funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+// 3. Se a peça passada for inválida, o código deve retornar uma mensagem de erro.
+// Exemplo: bishop (bispo) -> diagonals (diagonais)
+
