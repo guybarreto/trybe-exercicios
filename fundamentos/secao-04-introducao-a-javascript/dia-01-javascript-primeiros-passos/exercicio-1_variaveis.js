bfcad04ee5cd4aee642e7d0d7c84c0cf15472085
guybarreto <guybarreto@icloud.com>
2022-12-02 21:20:20 -0300

    Solve requirement 4

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1_variaveis.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1_variaveis.js
@@ -9,5 +9,9 @@ const birthCity = "Aracaju";
 let birthYear = 1986;
 
 // 4. Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
 // 5. Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
 // 6. Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
