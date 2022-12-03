3b8f0fa492f5ce8cb061c203fcaf43352b81e9ed
guybarreto <guybarreto@icloud.com>
2022-12-03 19:45:30 -0300

    Solve  exercicio-7_score-to-grade-converter.js

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-7_score-to-grade-converter.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-7_score-to-grade-converter.js
@@ -8,3 +8,25 @@
 //     *Porcentagem < 50 -> F
 // 2. O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
 
+const score = 40;
+let grade;
+
+if (score >= 0 && score <= 100) {
+    if (score >= 90) {
+        grade = "A";
+    } else if (score >= 80) {
+        grade = "B";
+    } else if (score >= 70) {
+        grade = "C";
+    } else if (score >= 60) {
+        grade = "D";
+    } else if (score >= 50) {
+        grade = "E";
+    } else {
+        grade = "F";
+    }
+} else {
+    console.log("Error! invalid score. / Erro! Nota inválida.");
+}
+
+console.log(grade);
