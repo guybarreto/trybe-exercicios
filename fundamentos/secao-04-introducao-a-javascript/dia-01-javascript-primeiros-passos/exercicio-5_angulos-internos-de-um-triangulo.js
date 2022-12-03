292c9542e8aab837c670dc308bcae2893dcb822a
guybarreto <guybarreto@icloud.com>
2022-12-03 17:52:32 -0300

    Solve exercicio-5_angulos-internos-de-um-triangulo.js

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-5_angulos-internos-de-um-triangulo.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-5_angulos-internos-de-um-triangulo.js
@@ -3,3 +3,19 @@
 //      * Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.
 //      * Um ângulo será considerado inválido se não tiver um valor positivo.
 
+const angle1 = 90;
+const angle2 = 40;
+const angle3 = 50;
+let isTriangle;
+
+if (angle1 <= 0 || angle2 <= 0 || angle3 <= 0) {
+    console.log("Erro! Todos os ângulos devem possuir valor positivo.");
+} else {
+    if (angle1 + angle2 + angle3 === 180) {
+        isTriangle = true;
+        console.log(isTriangle);
+    } else {
+        isTriangle = false;
+        console.log(isTriangle);
+    }
+}
