5e08310e0c9adbcae6d05f7dfbf34de632abe3de
guybarreto <guybarreto@icloud.com>
2022-12-02 23:15:27 -0300

    Solve requirement 3

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-4_operadores-logicos.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-4_operadores-logicos.js
@@ -12,5 +12,16 @@ let message = "";
 //      * Se o horário for maior ou igual a 14 e menor que 18, insira “Vamos fazer um bolo pro café da tarde?” na variável “message”.
 //      * Se o horário for maior ou igual a 11 e menor que 14, insira “Hora do almoço!!!” na variável “message”.
 //      * Se o horário estiver entre 4 e 11, insira “Hmmm, cheiro de café recém-passado” na variável “message”.
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir";
+} else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?";
+} else if (currentHour >= 11 && currentHour < 14) {
+    message = "Hora do almoço!!!";
+} else if (currentHour >= 4 && currentHour < 11) {
+    message = "Hmmm, cheiro de café recém-passado";
+}
 
 // 4. Agora imprima a variável message fora das suas condições.
