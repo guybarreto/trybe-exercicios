1fdf1f99e36b88eb8b2e17f0b3cd295653ea0e35
guybarreto <guybarreto@icloud.com>
2022-12-02 22:19:29 -0300

    Solve requirement 2

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-2_operacoes-aritmeticas.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-2_operacoes-aritmeticas.js
@@ -4,5 +4,7 @@
 const base = 5;
 
 // 2. Crie uma constante uma chamada heigth e atribua a ela o valor 8.
+const heigth = 8;
+
 // 3. Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela heigth. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
 // 4. Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
