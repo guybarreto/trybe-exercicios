34339063057af1233a5f37c773b9ca536462afc0
guybarreto <guybarreto@icloud.com>
2022-12-03 15:40:37 -0300

    Solve requirement 1 of exercicio-4_...

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-4_positive-or-negative.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-4_positive-or-negative.js
@@ -1,3 +1,11 @@
 // Exercício 04:
 // 1. Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+const value = -4;
 
+if (value > 0) {
+    console.log("positive");
+} else if (value < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
