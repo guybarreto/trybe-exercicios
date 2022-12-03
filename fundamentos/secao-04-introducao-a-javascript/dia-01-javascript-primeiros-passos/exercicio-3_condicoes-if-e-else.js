9de3b05a00652a5d7a90aecf43cfeb9132ba61c1
guybarreto <guybarreto@icloud.com>
2022-12-02 22:40:11 -0300

    Update exercicio-3_condicoes-if-e-else.js

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-3_condicoes-if-e-else.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-3_condicoes-if-e-else.js
@@ -5,5 +5,3 @@
 //      * Se a nota for maior ou igual a 80, imprima “Parabéns, você faz parte do grupo das pessoas aprovadas!”
 //      * Se a nota for menor que 80 e maior ou igual a 60, imprima “Você está na nossa lista de espera”
 //      * Se a nota for menor que 60, imprima “Infelizmente, você reprovou.”
-// 3. Crie uma estrutura condicional utilizando o if, else if e else para criar o seu algoritmo, e os operadores lógicos que se aplicam a cada situação.
-// 4. Altere o valor da nota para verificar se as condições que você implementou funcionam.
