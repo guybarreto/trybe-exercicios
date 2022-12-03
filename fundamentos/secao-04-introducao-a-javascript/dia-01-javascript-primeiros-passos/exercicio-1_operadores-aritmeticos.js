a86ef50097316143252828bfce7f017d95e9fff7
guybarreto <guybarreto@icloud.com>
2022-12-03 14:53:08 -0300

    Solve requirement 1

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1_operadores-aritmeticos.js
@@ -0,0 +1,19 @@
+// 1. Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. 
+const a = 10;
+const b = 5;
+
+// Escreva códigos para:
+// Adição (a + b)
+
+
+// Subtração (a - b)
+
+
+// Multiplicação (a * b)
+
+
+// Divisão (a / b)
+
+
+// Módulo (a % b)
+
