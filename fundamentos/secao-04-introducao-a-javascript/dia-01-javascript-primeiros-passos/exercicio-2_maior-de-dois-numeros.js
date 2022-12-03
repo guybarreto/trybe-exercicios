42f08e4ea70ee860b0fc9ee99c364dbe2da670ec
guybarreto <guybarreto@icloud.com>
2022-12-03 15:49:42 -0300

    Improve exercicio-2_maior-de-dois-numeros.js solution

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-2_maior-de-dois-numeros.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-2_maior-de-dois-numeros.js
@@ -1,12 +1,12 @@
 // Exercício 02:
 // 1. Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas constantes com os valores que serão comparados.
 const n1 = 7;
-const n2 = 10;
+const n2 = 5;
 
 if (n1 > n2) {
-    console.log(n1 + " é maior do que " + n2);
+    console.log("n1 (" + n1 + ") é maior do que n2 (" + n2 + ")");
 } else if (n2 > n1) {
-    console.log(n2 + " é maior que " + n1);
+    console.log("n2 (" + n2 + ") é maior do que n1 (" + n1 + ")");
 } else {
-    console.log(n1 + " e " + n2 + " são iguais");
+    console.log("n1 (" + n1 + ") e n2 (" + n2 + ") são iguais");
 }
