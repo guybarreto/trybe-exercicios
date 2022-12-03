261d125ed50313ff93f056e96653311f36bd8a1f
guybarreto <guybarreto@icloud.com>
2022-12-03 15:00:45 -0300

    Update exercicio-1_operadores-aritmeticos.js

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1_operadores-aritmeticos.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1_operadores-aritmeticos.js
@@ -1,4 +1,7 @@
-// 1. Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. 
+// Exercício 01: 
+// Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. 
+
+// 1. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. 
 const a = 10;
 const b = 5;
 
