c58aa64b4dc0434f6ff2b6d18a58adb9bfb0be8f
guybarreto <guybarreto@icloud.com>
2022-12-03 20:26:57 -0300

    Create exercicio-9_there-is-an-odd-number.js

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-9_there-is-an-odd-number.js
@@ -0,0 +1,4 @@
+// Exercício 09:
+// Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
+//     * Bonus: use somente um if.
+
