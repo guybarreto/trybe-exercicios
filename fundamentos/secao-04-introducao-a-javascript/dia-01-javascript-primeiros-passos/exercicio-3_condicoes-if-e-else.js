7d05a9f0c95aca98f6cfac32d936ac9e7ea91731
guybarreto <guybarreto@icloud.com>
2022-12-02 22:44:07 -0300

    Solve requirement 1

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-3_condicoes-if-e-else.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-3_condicoes-if-e-else.js
@@ -1,6 +1,8 @@
 // Exercício 03:
 
 // 1. Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
+const score = 90;
+
 // 2. Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se essa pessoa está na lista de espera. Para isso, considere as seguintes informações:
 //      * Se a nota for maior ou igual a 80, imprima “Parabéns, você faz parte do grupo das pessoas aprovadas!”
 //      * Se a nota for menor que 80 e maior ou igual a 60, imprima “Você está na nossa lista de espera”
