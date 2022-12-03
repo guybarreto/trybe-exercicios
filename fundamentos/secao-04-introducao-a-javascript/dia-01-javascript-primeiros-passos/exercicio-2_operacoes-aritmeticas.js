089bc040a111b6586be3ad88388cf188b806d41d
guybarreto <guybarreto@icloud.com>
2022-12-02 22:19:06 -0300

    Solve requirement 1

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-2_operacoes-aritmeticas.js
@@ -0,0 +1,8 @@
+// Exercício 02
+// Vamos fazer algumas operações simples para encontrarmos a área e o perímetro de um retângulo de base 5 e altura 8.
+// 1. Crie uma constante chamada base e atribua a ela o valor 5.
+const base = 5;
+
+// 2. Crie uma constante uma chamada heigth e atribua a ela o valor 8.
+// 3. Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela heigth. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+// 4. Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
