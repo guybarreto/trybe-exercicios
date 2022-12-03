fd4ee7dfdcd694bf99fdd0ade9c626894d0c9e64
guybarreto <guybarreto@icloud.com>
2022-12-03 18:58:54 -0300

    Create exercicio-7_score-to-grade-converter.js

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-7_score-to-grade-converter.js
@@ -0,0 +1,10 @@
+// Exercício 07:
+// 1. Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+//     *Porcentagem >= 90 -> A
+//     *Porcentagem >= 80 -> B
+//     *Porcentagem >= 70 -> C
+//     *Porcentagem >= 60 -> D
+//     *Porcentagem >= 50 -> E
+//     *Porcentagem < 50 -> F
+// 2. O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+
