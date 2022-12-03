b475cbf747bb965803f68c22d7297fb70624b87d
guybarreto <guybarreto@icloud.com>
2022-12-03 20:04:37 -0300

    Create exercicio-8_there-is-an-even-number.js

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-8_there-is-an-even-number.js
@@ -0,0 +1,4 @@
+// Exercício 08:
+// 1. Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
+//      * Bonus: use somente um if.
+
