1a016e31eb8412135a1470b1381a185d7d3d4c24
guybarreto <guybarreto@icloud.com>
2022-12-04 17:47:50 -0300

    Solve exercicio-8.js

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-7.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-7.js
@@ -1,11 +1,11 @@
 // Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let lowest = numbers[0];
+let lowestNumber = numbers[0];
 
 for (let index = 1; index < numbers.length; index++) {
-    if (numbers[index] < lowest) {
-        lowest = numbers[index];
+    if (numbers[index] < lowestNumber) {
+        lowestNumber = numbers[index];
     }
 }
-console.log(lowest);
+console.log('Lowest number: ' + lowestNumber);
