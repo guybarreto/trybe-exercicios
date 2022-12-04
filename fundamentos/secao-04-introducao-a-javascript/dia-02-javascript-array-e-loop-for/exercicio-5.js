c8335199d6232952dc62ee057fc8922669fbe662
guybarreto <guybarreto@icloud.com>
2022-12-04 17:35:53 -0300

    Solve exercicio-7.js

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-5.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-5.js
@@ -1,9 +1,9 @@
 // Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let largestNumber = null;
+let largestNumber = numbers[0];
 
-for (let index = 0; index < numbers.length; index++) {
+for (let index = 1; index < numbers.length; index++) {
     if (numbers[index] > largestNumber) {
         largestNumber = numbers[index];
     }
