f30d8e131bbb919e91d9773d29cfc84aa07377b2
guybarreto <guybarreto@icloud.com>
2022-12-04 15:02:29 -0300

    Solve exercicio-3_for-of-loop.js

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/para-fixar/exercicio-3_for-of-loop.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/para-fixar/exercicio-3_for-of-loop.js
@@ -2,3 +2,7 @@
 
 // 1. Utilize o for/of para imprimir os elementos da lista names com o console.log():
 let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (const name of names) {
+    console.log(name);
+}
