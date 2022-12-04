bc2760178400aa9f7520058148b345e2f4eac4a3
guybarreto <guybarreto@icloud.com>
2022-12-04 16:32:47 -0300

    Solve exercicio-2.js

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-2.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-2.js
@@ -1,4 +1,9 @@
 // Some todos os valores contidos no array e imprima o resultado;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
 
+for (let index = 0; index < numbers.length; index++) {
+    sum += numbers[index];    
+}
+console.log(sum);
