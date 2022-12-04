bbde5158c82c2057da7ab49ae47a98b7fe00fed2
guybarreto <guybarreto@icloud.com>
2022-12-04 14:14:32 -0300

    Create 'para-fixar' directory and exercicio-1_arrays.js

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/para-fixar/exercicio-1_arrays.js
@@ -0,0 +1,21 @@
+// Exercício 01:
+
+// 1. Obtenha o valor “Serviços” do array menu:
+
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = ;
+
+console.log(menuServices);
+
+// 2. Procure o índice do valor “Portfólio” do array menu:
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = ;
+
+console.log(indexOfPortfolio);
+
+// 3. Adicione o valor “Contato” no final do array menu:
+
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+console.log(menu);
