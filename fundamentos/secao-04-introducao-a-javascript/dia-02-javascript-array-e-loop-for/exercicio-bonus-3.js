98ab00fd237b4d314f2edfc44fbfeafa389d4496
guybarreto <guybarreto@icloud.com>
2022-12-04 21:22:51 -0300

    Solve exercicio-bonus-3.js

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-bonus-3.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-bonus-3.js
@@ -2,4 +2,13 @@
 // [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers =[];
 
+for (let index = 0; index < numbers.length; index++) {
+    if (index < numbers.length - 1) {
+        newNumbers.push(numbers[index] * numbers[index + 1]);
+    } else {
+        newNumbers.push(numbers[index] * 2);
+    }
+}
+console.log('newNumbers: [' + newNumbers.join(', ') + ']');
