4348877c4c3bec2e676a8aa5d7e3405402ec89ab
guybarreto <guybarreto@icloud.com>
2022-12-05 18:39:09 -0300

    Create exercicio-3.js

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-3.js
@@ -0,0 +1,6 @@
+// Considere o array de strings abaixo:
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
+
