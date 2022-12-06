596f9ae9ad428a3c1dc1a5555355a5e632a66206
guybarreto <guybarreto@icloud.com>
2022-12-05 23:48:00 -0300

    Create exercicio-bonus-6.js

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus-6.js
@@ -0,0 +1,3 @@
+// Faça um programa que diz se um número definido numa variável é primo ou não.
+// Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele por quaisquer outros números dá resto diferente de zero.
+
