b56cc81f31b295141e13b1ca534d270e44e84a09
guybarreto <guybarreto@icloud.com>
2022-12-05 22:57:50 -0300

    Solve exercicio-bonus-4.js

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus-4.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus-4.js
@@ -6,3 +6,24 @@
 //  ***
 // *****
 
+const n = 13;
+let numberOfLines;
+let array = [];
+
+if (n % 2 === 0) {
+    numberOfLines = n / 2;
+    for (let indexLines = 0; indexLines < numberOfLines; indexLines += 1) {
+        array[(n / 2) - indexLines] = '*';
+        array[((n / 2) + 1) + indexLines] = '*';
+        array.fill(' ', 0, (n / 2) - indexLines);
+        console.log(array.join(''));
+    }
+} else {
+    numberOfLines = (n / 2) + 0.5;
+    for (let indexLines = 0; indexLines < numberOfLines; indexLines += 1) {
+        array[((n / 2) + 0.5) - indexLines] = '*';
+        if (indexLines > 0) {array[((n / 2) + 1.5) + indexLines] = '*';}
+        array.fill(' ', 0, ((n / 2) + 0.5) - indexLines);
+        console.log(array.join(''));
+    }
+}
