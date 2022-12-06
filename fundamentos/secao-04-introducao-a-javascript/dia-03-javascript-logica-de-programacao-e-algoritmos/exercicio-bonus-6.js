507722c24b4abd67c939de7bf0494f99902fb2b3
guybarreto <guybarreto@icloud.com>
2022-12-05 23:56:41 -0300

    Solve exercicio-bonus-6.js

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus-6.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus-6.js
@@ -1,3 +1,23 @@
 // Faça um programa que diz se um número definido numa variável é primo ou não.
 // Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele por quaisquer outros números dá resto diferente de zero.
 
+let number = 39;
+let primeNumbers = [];
+
+for (index = 1; index <= number; index += 1) {
+  let numberOfDivisors = null;
+  for (index2 = 1; index2 <= number; index2 += 1) {
+    if (index > 1 && index % index2 === 0) {
+      numberOfDivisors += 1;
+    }
+  }
+  if (numberOfDivisors === 2) {
+    primeNumbers.push(index);
+  }
+  numberOfDivisors = 0;
+}
+if (primeNumbers.includes(number)) {
+    console.log(number + ' is a prime number');
+} else {
+    console.log(number + ' is NOT a prime number');
+}
