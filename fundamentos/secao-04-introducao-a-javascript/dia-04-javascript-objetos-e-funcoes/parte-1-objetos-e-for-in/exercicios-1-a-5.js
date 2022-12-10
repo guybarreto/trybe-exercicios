789f2412f5f625931280aa9dda6b74362fece13d
guybarreto <guybarreto@icloud.com>
2022-12-10 19:40:39 -0300

    Solve exercise 5

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/parte-1-objetos-e-for-in/exercicios-1-a-5.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/parte-1-objetos-e-for-in/exercicios-1-a-5.js
@@ -31,14 +31,29 @@ for (const key in info) {
     console.log(key);
   }
 
-// 4. Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
-// Margarida
-// Pato Donald
-// Namorada do personagem principal nos quadrinhos do Pato Donald
-// Sim
+// // 4. Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// // Margarida
+// // Pato Donald
+// // Namorada do personagem principal nos quadrinhos do Pato Donald
+// // Sim
 
 for (const key in info) {
     console.log(info[key]);
   }
 
-  
+// 5. Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+}
+
+for (const key in info) {
+  if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(`${info[key]} e ${info2[key]}`);
+  }
+}
