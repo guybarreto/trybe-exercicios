767db02e39d55da0a4b596f1c33bf8c2cbb65f60
guybarreto <guybarreto@icloud.com>
2022-12-18 18:36:57 -0300

    Solve exercicio bonus 2

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/parte-3-bonus/exercicio-bonus-2.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/parte-3-bonus/exercicio-bonus-2.js
@@ -7,3 +7,19 @@ let vector = [
   [3, 4, 5, 6],
   [7, 8, 9, 10],
 ];
+
+const isPair = (number) => number % 2 === 0;
+
+const arrayOfNumbers = (vector) => {
+  let pairNumbers = [];
+  for (let index = 0; index < vector.length; index++) {
+    for (let index2 = 0; index2 < vector[index].length; index2++) {
+      const number = vector[index][index2];
+      if (isPair(number)) {
+        pairNumbers.push(number);
+      }
+    }
+  }
+  return pairNumbers;
+};
+console.log(arrayOfNumbers(vector)); // [ 2, 4, 6, 8, 10 ]
