47d52dae4fc799742227f6c94af7cef2c1b716fd
guybarreto <guybarreto@icloud.com>
2022-12-18 20:02:27 -0300

    Solve question 1 of exercicio bonus 4

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/parte-3-bonus/exercicio-bonus-4.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/parte-3-bonus/exercicio-bonus-4.js
@@ -31,6 +31,11 @@ let moradores = {
   ],
 };
 
-// 1. Acesse as chaves nome, sobrenome, andar e apartamento do último morador do blocoDois e faça um console.log no seguinte formato: “O morador do bloco 2 de nome Zoey Brooks mora no 1° andar, apartamento 101”.
+// 1. Acesse as chaves nome, sobrenome, andar e apartamento do último morador do blocoDois e faça um console.log no seguinte formato: “O morador do bloco 2, de nome Zoey Brooks, mora no 1° andar, apartamento 101.”.
+let blocoDoisResidents = moradores.blocoDois;
+let lastBlocoDoisResident = blocoDoisResidents[blocoDoisResidents.length - 1];
+console.log(
+  `O morador do bloco 2, de nome ${lastBlocoDoisResident.nome} ${lastBlocoDoisResident.sobrenome}, mora no ${lastBlocoDoisResident.andar} andar, apartamento ${lastBlocoDoisResident.apartamento}.`
+);
 
 // 2. Utilize o for para imprimir o nome completo de todos os moradores do bloco 1, acessando suas chaves nome e sobrenome. Depois faça o mesmo para os moradores do bloco 2.
