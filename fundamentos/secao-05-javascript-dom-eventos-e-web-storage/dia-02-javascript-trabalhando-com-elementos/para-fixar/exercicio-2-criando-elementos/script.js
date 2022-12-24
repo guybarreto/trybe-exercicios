ff1470666a0c7cebc19adfa3388d5b7cc2458c1b
guybarreto <guybarreto@icloud.com>
2022-12-24 19:19:36 -0300

    Solve requirement 1

--- fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/para-fixar/exercicio-2-criando-elementos/script.js
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/para-fixar/exercicio-2-criando-elementos/script.js
@@ -1,4 +1,9 @@
 // 1. Crie um irmão para elementoOndeVoceEsta.
+const newElement1 = document.createElement('section');
+newElement1.id = 'irmaoDeElementoOndeVoceEsta';
+const pai = document.getElementById('elementoOndeVoceEsta').parentElement;
+pai.appendChild(newElement1);
+
 // 2. Crie um filho para elementoOndeVoceEsta.
 // 3. Crie um filho para primeiroFilhoDoFilho.
 // 4. A partir desse filho criado, acesse terceiroFilho.
