0c5a300041a9a6798b18a5727bc0f25b5fa5661c
guybarreto <guybarreto@icloud.com>
2022-12-27 17:42:56 -0300

    Solve requirement 1

--- fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio-1/script.js
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio-1/script.js
@@ -22,3 +22,24 @@ const createDaysOfTheWeek = () => {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+// 1.
+const decemberDaysList = [
+  27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+  19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+const ulDays = document.getElementById('days');
+
+for (let index = 0; index < decemberDaysList.length; index += 1) {
+  const day = decemberDaysList[index];
+  const newLiTag = document.createElement('li');
+  newLiTag.innerHTML = day;
+  newLiTag.className = 'day';
+
+  if (day == 24 || day == 25 || day == 31) {
+    newLiTag.className += 'holiday';
+  }
+  if (day == 4 || day == 11 || day == 18 || day == 25) {
+    newLiTag.className += 'friday';
+  }
+  ulDays.appendChild(newLiTag);
+}
