052c6d881dc67a761e4b935a64a72228fa72c502
guybarreto <guybarreto@icloud.com>
2022-12-27 14:03:20 -0300

    Solve requirement 4

--- fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/para-fixar/main.js
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/para-fixar/main.js
@@ -6,6 +6,7 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // - Copie esse arquivo e edite apenas ele;
 // - Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// R: Devido à propriedade "transform: translateY(-20px);" inserida na class tech em style.css
 
 // 1. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
@@ -46,6 +47,14 @@ function redirectToPortfolio() {
 
 // 4. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+meuTop3.addEventListener('mouseenter', changeColor);
+meuTop3.addEventListener('mouseleave', changeColor);
+
+function changeColor(event) {
+  event.type === 'mouseenter'
+    ? (meuTop3.style.color = 'yellow')
+    : (meuTop3.style.color = 'white');
+}
 
 // Segue abaixo um exemplo do uso de event.target:
 
