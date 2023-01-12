569bc2548505d0030ad3a5393dc08017ad7b086e
guybarreto <guybarreto@icloud.com>
2023-01-12 15:08:37 -0300

    Solve requirement 13

--- fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercicio-1/script.js
+++ fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercicio-1/script.js
@@ -1,5 +1,17 @@
 const submitBtn = document.getElementById('submitBtn');
+const nameInput = document.getElementById('name-input');
+const emailInput = document.getElementById('email-input');
+const answerTextArea = document.getElementsByTagName('textArea')[0];
 
 submitBtn.addEventListener('click', (event) => {
-  event.preventDefault();
+  //   event.preventDefault();
+  const nameInputCheck = nameInput.value.length >= 10 && nameInput.value.length <= 40;
+  const emailInputCheck = emailInput.value.length >= 10 && emailInput.value.length <= 50;
+  const answerTextAreaCheck = answerTextArea.value.length <= 500;
+
+  if (nameInputCheck && emailInputCheck && answerTextAreaCheck) {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  } else {
+    alert('Dados Inválidos');
+  }
 });
