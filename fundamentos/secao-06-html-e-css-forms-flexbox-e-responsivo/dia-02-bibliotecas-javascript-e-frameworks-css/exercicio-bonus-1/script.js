4374cab41a43f427e52fead516db941857a7b03a
guybarreto <guybarreto@icloud.com>
2023-01-21 04:34:48 -0300

    Fix errors

--- fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/exercicio-bonus-1/script.js
+++ fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/exercicio-bonus-1/script.js
@@ -3,33 +3,80 @@ var picker = new Pikaday({ field: document.getElementById('datepicker') });
 var picker = new Pikaday({
   field: document.getElementById('datepicker'),
   format: 'D MMM YYYY',
-  onSelect: function () {
-    console.log(this.getMoment().format('Do MMMM YYYY'));
+
+  onSelect: function() {
+    document.getElementById('datepicker').value = this.getMoment().format('DD/MM/YYYY');
   },
 });
 
-const validate = new window.JustValidate('#form');
-const validation = new JustValidate('#form');
+const validate = new window.JustValidate(
+  '#form', 
+  {
+    errorFieldCssClass: 'is-invalid',
+    errorFieldStyle: {
+      border: '1px solid red',
+    },
+    errorLabelCssClass: 'is-label-invalid',
+    errorLabelStyle: {
+      color: 'red',
+      textDecoration: 'underlined',
+    },
+    focusInvalidField: true,
+    lockForm: true,
+    tooltip: {
+      position: 'top',
+    },
+  },
+);
 
-validation
+validate
   .addField('#name', [
     {
       rule: 'minLength',
-      value: 3,
+      value: 10,
     },
     {
       rule: 'maxLength',
-      value: 30,
+      value: 40,
     },
+    {
+      rule: 'required',
+      errorMessage: 'Digite seu nome'
+    }
   ])
   .addField('#email', [
     {
+      rule: 'minLength',
+      value: 10,
+    },
+    {
+      rule: 'maxLength',
+      value: 50,
+    },
+    {
       rule: 'required',
-      errorMessage: 'Email is required',
+      errorMessage: 'Digite seu e-mail',
     },
     {
       rule: 'email',
-      errorMessage: 'Email is invalid!',
+      errorMessage: 'E-mail inválido',
+    },
+  ])
+  .addField('#answer', [
+    {
+      rule: 'maxLength',
+      value: 500,
+    }
+  ])
+  .addField('#datepicker', [
+    {
+      rule: 'required',
+      errorMessage: 'Informe uma data'
+    }
+  ])
+  .addField('#agree2', [
+        {
+      rule: 'required',
+      errorMessage: 'Campo obrigatório'
     },
   ]);
-console.log(moment().format('DD/MM/YYYY'));
