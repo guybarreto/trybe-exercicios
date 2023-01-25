89243776ab2b641ec4a69c5c9e3901cf2d717e26
guybarreto <guybarreto@icloud.com>
2023-01-25 03:46:02 -0300

    Creates exercicio-bonus directory and script.js

---
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/exercicio-bonus/script.js
@@ -0,0 +1,5 @@
+/* Exercício Bônus
+1. Utilizando o objeto (allLesson), crie uma função para contar quantos estudantes assistiram às aulas de Matemática;
+
+2. Utilizando o objeto (allLesson), crie uma função que deva retornar um objeto que represente o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes.
+*/
