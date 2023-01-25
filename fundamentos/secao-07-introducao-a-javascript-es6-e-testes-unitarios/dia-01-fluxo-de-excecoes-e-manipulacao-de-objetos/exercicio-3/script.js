b695d1aac94b9753771a4829cc5f6cb7d0c5a31a
guybarreto <guybarreto@icloud.com>
2023-01-25 01:30:55 -0300

    Solves requirement 1

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/exercicio-3/script.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/exercicio-3/script.js
@@ -17,3 +17,19 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
+
+const addNightShift = (object, key, value) => {
+    object[key] = value;
+}
+addNightShift(lesson2, 'turno', 'noite');
+
+
+/* Requisitos
+1. Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+2. Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+3. Crie uma função para mostrar o tamanho de um objeto.
+4. Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+5. Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
+6. Usando o objeto criado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas.
+7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+8. Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles o objeto, o nome da chave e o valor da chave. */
