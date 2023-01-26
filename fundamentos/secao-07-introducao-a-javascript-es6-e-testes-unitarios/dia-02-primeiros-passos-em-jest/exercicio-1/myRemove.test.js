6e08f1bbc2f36a0acb13e02cffd7748800005bfd
guybarreto <guybarreto@icloud.com>
2023-01-25 23:27:32 -0300

    Install jest and solves test1

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio-1/myRemove.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio-1/myRemove.test.js
@@ -3,4 +3,10 @@
     2. Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4];
     3. Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.
 */
+const myRemove = require('./myRemove');
 
+describe('myRemove function', () => {
+    test('myRemove([1, 2, 3, 4], 3) returns [1, 2, 4]', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
+    });
+});
