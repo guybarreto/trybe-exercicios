d3db46d83dc7b3d03f2e80298a0f6f25d0888a11
guybarreto <guybarreto@icloud.com>
2023-01-25 23:53:17 -0300

    Creates exercicio-3 directory, encodeDecode.js and encodeDecode.test.js

---
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio-3/encodeDecode.test.js
@@ -0,0 +1,8 @@
+/*
+Para as funções encode e decode, crie os seguintes testes em Jest:
+    1. Teste se encode e decode são funções;
+    2. Para a função encode, teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente;
+    3. Para a função decode, teste se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente;
+    4. Teste se as demais letras/números não são convertidos para cada caso;
+    5. Teste se a string que é retornada pelas funções tem o mesmo número de caracteres que a string passada como parâmetro.
+*/
