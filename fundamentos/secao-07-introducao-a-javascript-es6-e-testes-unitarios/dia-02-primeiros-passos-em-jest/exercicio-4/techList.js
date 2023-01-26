bc2709210ef532494c2570c6912446698bad574b
guybarreto <guybarreto@icloud.com>
2023-01-26 01:37:19 -0300

    Solves exercicio-4 requirement

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio-4/techList.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio-4/techList.js
@@ -1,3 +1,20 @@
+const checkTechArray = (techArray) => {
+    if (techArray.length === 0) {
+        throw new Error('Vazio!');
+    }
+}
 
+const techList = (techArray, name) => {
+    const sortedObjArray = [];
+    try {
+        checkTechArray(techArray);
+        techArray.sort().forEach(tech => {
+            sortedObjArray.push({'tech': tech, 'name': name})
+        });
+        return sortedObjArray;
+    } catch (error) {
+        return error.message;
+    }
+}
 
 module.exports = techList;
