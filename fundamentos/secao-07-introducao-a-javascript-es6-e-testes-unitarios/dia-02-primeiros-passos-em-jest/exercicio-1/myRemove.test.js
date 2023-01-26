913cac674603fb28323595f0553fe7844be1d21a
guybarreto <guybarreto@icloud.com>
2023-01-25 23:28:00 -0300

    Solves test3

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio-1/myRemove.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio-1/myRemove.test.js
@@ -12,4 +12,7 @@ describe('myRemove function', () => {
     test('myRemove([1, 2, 3, 4], 3) does not return [1, 2, 3, 4]', () => {
         expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4])
     });
+    test('myRemove([1, 2, 3, 4], 5) returns [1, 2, 3, 4]', () => {
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
+    });
 });
