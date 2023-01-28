da20e5b9029dd08530d98a8180d90f30d09093f1
guybarreto <guybarreto@icloud.com>
2023-01-28 02:23:43 -0300

    Creates "sum(0, 0) returns 0" test

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicio-1/sum.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/exercicio-1/sum.test.js
@@ -3,5 +3,8 @@ const sum = require('./sum');
 describe('A função sum', () => {
     test('O retorno de sum(4, 5) é 9', () => {
         expect(sum(4, 5)).toBe(9);
-    })
+    });
+    test('O retorno de sum(0, 0) é 0', () => {
+        expect(sum(0, 0)).toBe(0);
+    });
 })
