c348d757877a86104cdb44af5f2ca07ab3aaa225
guybarreto <guybarreto@icloud.com>
2023-01-28 01:27:57 -0300

    Implements the "Lança um erro quando não recebe parâmetros" test

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
@@ -6,7 +6,9 @@ describe('a função createItem', () => {
   it('utiliza zero como quantidade padrão', () => {
     expect(createItem('banana', 'kg', 1.99)).toEqual({ name: 'banana', unit: 'kg', price: 1.99, quantity: 0 })
   });
-  it.todo('Lança um erro quando não recebe parâmetros');
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => createItem()).toThrow(Error);
+  });
   it.todo('Lança um erro se o nome do item não é uma string');
   it.todo('Lança um erro se o preço é negativo');
   it.todo('Lança um erro se o preço é zero');
