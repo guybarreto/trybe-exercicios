46f1be13efb4e8057dc28f74168077a2a4805189
guybarreto <guybarreto@icloud.com>
2023-01-28 01:42:08 -0300

    Implements the "Lança um erro se o preço é zero" test

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
@@ -15,5 +15,7 @@ describe('a função createItem', () => {
   it('Lança um erro se o preço é negativo', () => {
     expect(() => createItem('banana', 'kg', -0.01, 20)).toThrow('O preço do item deve ser maior que zero');
   });
-  it.todo('Lança um erro se o preço é zero');
+  it('Lança um erro se o preço é zero', () => {
+    expect(() => createItem('banana', 'kg', -0.00, 20)).toThrow('O preço do item deve ser maior que zero');
+  });
 });
