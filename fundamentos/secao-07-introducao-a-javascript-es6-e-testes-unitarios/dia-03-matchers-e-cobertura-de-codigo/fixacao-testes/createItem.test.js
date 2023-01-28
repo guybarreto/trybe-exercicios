db6d2c4b1ae9489e0172718e21e02134e2f649fa
guybarreto <guybarreto@icloud.com>
2023-01-28 01:25:31 -0300

    Implements the "utiliza zero como quantidade padrão" test

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
@@ -3,7 +3,9 @@ describe('a função createItem', () => {
   it('cria um item válido', () => {
     expect(createItem('banana', 'kg', 1.99, 20)).toEqual({ name: 'banana', unit: 'kg', price: 1.99, quantity: 20 });
   });
-  it.todo('utiliza zero como quantidade padrão');
+  it('utiliza zero como quantidade padrão', () => {
+    expect(createItem('banana', 'kg', 1.99)).toEqual({ name: 'banana', unit: 'kg', price: 1.99, quantity: 0 })
+  });
   it.todo('Lança um erro quando não recebe parâmetros');
   it.todo('Lança um erro se o nome do item não é uma string');
   it.todo('Lança um erro se o preço é negativo');
