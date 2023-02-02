610489298be5598c28765c31d39b8d51c9d22ed9
guybarreto <guybarreto@icloud.com>
2023-02-02 16:24:06 -0300

    Solve exercicio-4-4.js

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio-4-organizando-uma biblioteca/exercicio-4-1.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio-4-organizando-uma biblioteca/exercicio-4-1.js
@@ -1,14 +1,5 @@
 const books = require('../exercicio-4-organizando-uma biblioteca/library-database')
 
 const expectedResult = 'Stephen King';
-const authorBornIn1947 = (booksArray) => booksArray.find((book) => book.author.birthYear === 1947);
-console.log(authorBornIn1947(books));
-/* output:
-{
-  id: 5,
-  name: 'A Coisa',
-  genre: 'Terror',
-  author: { name: 'Stephen King', birthYear: 1947 },
-  releaseYear: 1986
-} 
-*/
+const authorBornIn1947 = (booksArray) => booksArray.find((book) => book.author.birthYear === 1947).author.name;
+console.log(authorBornIn1947(books)); // Stephen King
--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio-4-organizando-uma biblioteca/exercicio-4-4.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio-4-organizando-uma biblioteca/exercicio-4-4.js
@@ -1,5 +1,9 @@
+const books = require('./library-database');
+
 const expectedResult = false;
 
-function everyoneWasBornOnSecXX() {
-  // escreva seu código aqui
-}
+function everyoneWasBornOnSecXX(booksArray) {
+  return booksArray.every((book) => book.author.birthYear >= 1901 && book.author.birthYear <= 2000)
+  }
+
+console.log(everyoneWasBornOnSecXX(books)); // false
