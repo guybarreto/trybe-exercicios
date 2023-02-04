5f3e545633c9889eb3bf1fd30352f6effcc74276
guybarreto <guybarreto@icloud.com>
2023-02-04 16:44:56 -0300

    Create warriorDmg.js and implement warriorDmg function

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio-bonus-game-actions-simulator/dragonDmg.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio-bonus-game-actions-simulator/dragonDmg.js
@@ -1,9 +1,8 @@
 const battleMembers = require('./stats-database');
 
 const dragonDmg = () => {
-    const maxDmg = battleMembers.dragon.strength;
     const minDmg = 15;
+    const maxDmg = battleMembers.dragon.strength;
     const dmg = Math.round(Math.random() * (maxDmg - minDmg + 1) + minDmg); // +1 to make the maxDmg inclusive
-
     return dmg;
 }
---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio-bonus-game-actions-simulator/warriorDmg.js
@@ -0,0 +1,8 @@
+const battleMembers = require('./stats-database');
+
+const warriorDmg = () => {
+    const minDmg = battleMembers.warrior.strength;
+    const maxDmg = minDmg * battleMembers.warrior.weaponDmg;
+    const dmg = Math.floor(Math.random() * (maxDmg - minDmg + 1) + minDmg); // +1 to make the maxDmg inclusive
+    return dmg;
+}
