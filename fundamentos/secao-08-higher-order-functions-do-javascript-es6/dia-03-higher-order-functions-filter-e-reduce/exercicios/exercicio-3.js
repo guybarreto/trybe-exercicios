b368f224ffadd845696a39fe7fffd5b237c5574b
guybarreto <guybarreto@icloud.com>
2023-02-06 07:01:01 -0300

    Create and solve exercicio-4.js

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicios/exercicio-3.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicios/exercicio-3.js
@@ -1,14 +1,15 @@
 // Faça uma função que retorne os nomes dos livros, dado o ano de nascimento das pessoas autoras.
 const books = require('./library-database');
 
-const expectedResult = [ 'Fundação', 'Duna' ];
+const expectedResult = ['Fundação', 'Duna'];
 
-const booksByAuthorBirthYear = (birthYear) => books.filter((book) =>
-    book.author.birthYear === birthYear)
-        .reduce((acc, book) => {
-            acc.push(book.name);
-            return acc;
-        }, [])
+const booksByAuthorBirthYear = (birthYear) =>
+  books
+    .filter((book) => book.author.birthYear === birthYear)
+    .reduce((acc, book) => {
+      acc.push(book.name);
+      return acc;
+    }, []);
 
 const result = booksByAuthorBirthYear(1920);
 
