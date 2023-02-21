36711e884ce85414713de39c3d34b3bffc6679b4
guybarreto <guybarreto@icloud.com>
2023-02-21 12:44:46 -0300

    Solve exercise function

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/exercicios/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/exercicios/src/main.js
@@ -1,2 +1,19 @@
 import './style.css';
-import { validator } from 'validator';
+import validator from 'validator';
+
+const textInput = document.getElementById('textInput');
+const selectList = document.getElementById('selectList');
+const validationBtn = document.getElementById('validationBtn');
+const answer = document.getElementById('answer');
+
+validationBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const campos = {
+    cpf: validator.isTaxID(textInput.value, 'pt-BR'),
+    email: validator.isEmail(textInput.value),
+    hexColor: validator.isHexColor(textInput.value),
+    postalCode: validator.isPostalCode(textInput.value, 'BR'),
+    url: validator.isURL(textInput.value),
+  };
+  answer.innerHTML = `A validação retornou ${campos[selectList.value]}.`;
+});
