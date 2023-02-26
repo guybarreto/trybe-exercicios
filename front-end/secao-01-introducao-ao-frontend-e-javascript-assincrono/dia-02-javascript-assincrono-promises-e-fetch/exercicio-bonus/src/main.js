68d5340faea65e04bf25dc7f139f112d0ab128fa
guybarreto <guybarreto@icloud.com>
2023-02-25 23:14:06 -0300

    Concludes Select Pet main.js

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/exercicio-bonus/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/exercicio-bonus/src/main.js
@@ -1,1 +1,34 @@
 import './style.css';
+
+// Getting html elements
+const dogBtn = document.getElementById('dog-btn');
+const catBtn = document.getElementById('cat-btn');
+const surpriseBtn = document.getElementById('surprise-btn');
+const petImage = document.getElementById('pet-img');
+
+dogBtn.addEventListener('click', () => {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then((response) => response.json())
+    .then((data) => {
+      petImage.src = data.message;
+    });
+});
+
+catBtn.addEventListener('click', () => {
+  fetch('https://aws.random.cat/meow')
+    .then((response) => response.json())
+    .then((data) => {
+      petImage.src = data.file;
+    });
+});
+
+surpriseBtn.addEventListener('click', () => {
+  Promise.any([
+    fetch('https://dog.ceo/api/breeds/image/random'),
+    fetch('https://aws.random.cat/meow'),
+  ])
+    .then((response) => response.json())
+    .then((data) => {
+      petImage.src = data.file || data.message;
+    });
+});
